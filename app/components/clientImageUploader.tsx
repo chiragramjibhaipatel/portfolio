4237fe2e45e9ee8b0e5f83a0479427fe319db835
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2024-08-20 21:54:04 +0530

    fix the image upload issue

--- app/components/clientImageUploader.tsx
+++ app/components/clientImageUploader.tsx
@@ -1,14 +1,29 @@
-import {useFetcher} from "@remix-run/react";
-import {useCallback, useEffect, useState} from "react";
-import {BlockStack, Button, Card, DropZone, InlineGrid, InlineStack, Spinner, Text} from "@shopify/polaris";
+import { useFetcher } from "@remix-run/react";
+import { useCallback, useEffect, useState } from "react";
+import {
+  BlockStack,
+  Button,
+  Card,
+  DropZone,
+  InlineGrid,
+  InlineStack,
+  Spinner,
+  Text,
+} from "@shopify/polaris";
 
-export function ClientImageUploader({imageUrl, handleSetImageUrl}: {
-  imageUrl?: string | null,
-  handleSetImageUrl: (imageUrl: string) => void
+export function ClientImageUploader({
+  imageUrl,
+  handleSetImageUrl,
+}: {
+  imageUrl?: string | null;
+  handleSetImageUrl: (imageUrl: string) => void;
 }) {
-
-  let fetcherFileUpload = useFetcher<{ imageUrl: string }>({key: "fileUpload"});
-  const fetcherFileUploadLoading = ["loading", "submitting"].includes(fetcherFileUpload.state);
+  let fetcherFileUpload = useFetcher<{ imageUrl: string }>({
+    key: "fileUpload",
+  });
+  const fetcherFileUploadLoading = ["loading", "submitting"].includes(
+    fetcherFileUpload.state,
+  );
 
   const [file, setFile] = useState<File>();
   const [openFileDialog, setOpenFileDialog] = useState(false);
@@ -19,45 +34,52 @@ export function ClientImageUploader({imageUrl, handleSetImageUrl}: {
   );
 
   useEffect(() => {
-    if (fetcherFileUpload.state === "idle" && fetcherFileUpload.data?.imageUrl) {
-      console.log("File uploaded successfully", fetcherFileUpload
-        .data);
+    if (
+      fetcherFileUpload.state === "idle" &&
+      fetcherFileUpload.data?.imageUrl
+    ) {
+      console.log("File uploaded successfully", fetcherFileUpload.data);
       handleSetImageUrl(fetcherFileUpload.data.imageUrl);
-      fetcherFileUpload.load("/api/reset")
+      fetcherFileUpload.load("/api/reset");
     }
-  }, [fetcherFileUpload])
+  }, [fetcherFileUpload]);
 
   const handleDropZoneDrop = useCallback(
     (_dropFiles: File[], acceptedFiles: File[], _rejectedFiles: File[]) => {
       console.log("Accepted Files: ", acceptedFiles);
       let formData = new FormData();
       formData.append("file", acceptedFiles[0]);
-      fetcherFileUpload.submit(formData, {action: "/api/image/upload", method: "POST", encType: "multipart/form-data"});
-      setFile(acceptedFiles[0])
+      fetcherFileUpload.submit(formData, {
+        action: "/api/image/upload",
+        method: "POST",
+        encType: "multipart/form-data",
+      });
+      setFile(acceptedFiles[0]);
     },
     [],
   );
 
-  const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+  const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
 
-  const fileUpload = (!file && !imageUrl) && <DropZone.FileUpload actionTitle={"Upload"}/>;
-  const uploadedFile = fetcherFileUploadLoading ?
-    (
-      <InlineStack blockAlign={"center"} align={"center"}>
-        <Spinner accessibilityLabel="Image is being uploaded" size="small"/>
-      </InlineStack>
-    ) :
-    (
-      <InlineStack>
-        <img
-          style={{maxWidth: "100%", borderRadius: "7px"}}
-          src={
-            (file && validImageTypes.includes(file.type))
-              ? window.URL.createObjectURL(file)
-              : imageUrl ?? ""
-          }/>
-      </InlineStack>
-    );
+  const fileUpload = !file && !imageUrl && (
+    <DropZone.FileUpload actionTitle={"Upload"} />
+  );
+  const uploadedFile = fetcherFileUploadLoading ? (
+    <InlineStack blockAlign={"center"} align={"center"}>
+      <Spinner accessibilityLabel="Image is being uploaded" size="small" />
+    </InlineStack>
+  ) : (
+    <InlineStack>
+      <img
+        style={{ maxWidth: "100%", borderRadius: "7px" }}
+        src={
+          file && validImageTypes.includes(file.type)
+            ? window.URL.createObjectURL(file)
+            : (imageUrl ?? "")
+        }
+      />
+    </InlineStack>
+  );
 
   return (
     <Card>
@@ -66,11 +88,19 @@ export function ClientImageUploader({imageUrl, handleSetImageUrl}: {
           <Text as="h2" variant="bodyMd">
             Logo/Image
           </Text>
-          {(file || imageUrl) && <Button onClick={toggleOpenFileDialog} variant={"plain"}>Change</Button>}
+          {(file || imageUrl) && (
+            <Button onClick={toggleOpenFileDialog} variant={"plain"}>
+              Change
+            </Button>
+          )}
         </InlineGrid>
-        <DropZone disabled={fetcherFileUploadLoading} allowMultiple={false} onDrop={handleDropZoneDrop}
-                  openFileDialog={openFileDialog}
-                  onFileDialogClose={toggleOpenFileDialog}>
+        <DropZone
+          disabled={fetcherFileUploadLoading}
+          allowMultiple={false}
+          onDrop={handleDropZoneDrop}
+          openFileDialog={openFileDialog}
+          onFileDialogClose={toggleOpenFileDialog}
+        >
           {uploadedFile}
           {fileUpload}
         </DropZone>
--- app/routes/app.clients.$id.tsx
+++ app/routes/app.clients.$id.tsx
@@ -12,24 +12,30 @@ import {
   Text,
   TextField,
 } from "@shopify/polaris";
-import type {ActionFunctionArgs, LoaderFunctionArgs} from "@remix-run/node";
-import { json, redirect} from "@remix-run/node";
-import {z} from "zod";
-import {useFetcher, useLoaderData} from "@remix-run/react";
-import {authenticate} from "~/shopify.server";
-import db from "../db.server"
-import {useCallback, useEffect, useState} from "react";
-import {ClientImageUploader} from "~/components/clientImageUploader";
-import {useIsNew} from "~/hooks/useIsNew";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { z } from "zod";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { authenticate } from "~/shopify.server";
+import db from "../db.server";
+import { useCallback, useEffect, useState } from "react";
+import { ClientImageUploader } from "~/components/clientImageUploader";
+import { useIsNew } from "~/hooks/useIsNew";
 
 const ClientSchema = z.object({
-  name: z.string().min(3, {message: "Name must be at least 3 characters long"}),
-  company: z.string().min(3, {message: "Company must be at least 3 characters long"}).nullable().optional(),
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  company: z
+    .string()
+    .min(3, { message: "Company must be at least 3 characters long" })
+    .nullable()
+    .optional(),
   about: z.string().nullable().optional(),
   email: z.string().email().nullable().optional(),
   stores: z.array(z.string()).optional(),
-  imageUrl: z.string().nullable().optional()
-})
+  imageUrl: z.string().nullable().optional(),
+});
 
 // Define the type for form errors
 type FormErrors = {
@@ -37,18 +43,18 @@ type FormErrors = {
   fieldErrors?: Record<string, string[]>;
 };
 
-export const loader = async ({request, params}: LoaderFunctionArgs) => {
-  const {session} = await authenticate.admin(request);
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const { session } = await authenticate.admin(request);
 
-  const {id} = params;
+  const { id } = params;
   if (id === "add") {
-    return json({client: null});
+    return json({ client: null });
   }
   console.log("Existing Client id: ", id);
   const client = await db.client.findUnique({
     where: {
       id: id,
-      sessionId: session.id
+      sessionId: session.id,
     },
     select: {
       name: true,
@@ -56,27 +62,30 @@ export const loader = async ({request, params}: LoaderFunctionArgs) => {
       about: true,
       email: true,
       stores: true,
-      imageUrl: true
-    }
-  })
+      imageUrl: true,
+    },
+  });
   if (!client) {
-    throw new Response("Client not found", {status: 404});
+    throw new Response("Client not found", { status: 404 });
   }
   console.log("Client: ", client);
-  return json({client});
+  return json({ client });
 };
 
-export const action = async ({request, params}: ActionFunctionArgs) => {
-  const {session} = await authenticate.admin(request);
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  const { session } = await authenticate.admin(request);
   console.log("params action", params);
   const formData = await request.json();
   const result = ClientSchema.safeParse(formData);
 
   if (!result.success) {
-    return json({status: "error", errors: result.error.flatten()}, {status: 400});
+    return json(
+      { status: "error", errors: result.error.flatten() },
+      { status: 400 },
+    );
   }
 
-  const {name, company, about, email, stores, imageUrl} = result.data;
+  const { name, company, about, email, stores, imageUrl } = result.data;
   let client;
   if (params.id === "add") {
     console.log("Creating new client");
@@ -88,8 +97,8 @@ export const action = async ({request, params}: ActionFunctionArgs) => {
         email,
         sessionId: session.id,
         stores,
-        imageUrl
-      }
+        imageUrl,
+      },
     });
     return redirect(`/app/clients/${client.id}`);
   } else {
@@ -97,7 +106,7 @@ export const action = async ({request, params}: ActionFunctionArgs) => {
     client = await db.client.update({
       where: {
         id: params.id,
-        sessionId: session.id
+        sessionId: session.id,
       },
       data: {
         name,
@@ -105,7 +114,7 @@ export const action = async ({request, params}: ActionFunctionArgs) => {
         about,
         email,
         stores,
-        imageUrl
+        imageUrl,
       },
       select: {
         name: true,
@@ -113,30 +122,26 @@ export const action = async ({request, params}: ActionFunctionArgs) => {
         about: true,
         email: true,
         stores: true,
-        imageUrl: true
-      }
+        imageUrl: true,
+      },
     });
-    return json({status: "success", client}, {status: 200});
+    return json({ status: "success", client }, { status: 200 });
   }
-}
-
+};
 
 export default function NewClient() {
   let data = useLoaderData<typeof loader>();
   const [clientData, setClientData] = useState(data.client);
   const fetcher = useFetcher();
   const isNew = useIsNew(fetcher);
-  const [formErrors, setFormErrors] = useState<FormErrors | undefined>(undefined);
-  const [storeUrl, setStoreUrl] = useState<string | undefined>()
-  const [storeUrlFocus, setStoreUrlFocus] = useState<boolean | undefined>()
+  const [formErrors, setFormErrors] = useState<FormErrors | undefined>(
+    undefined,
+  );
+  const [storeUrl, setStoreUrl] = useState<string | undefined>();
+  const [storeUrlFocus, setStoreUrlFocus] = useState<boolean | undefined>();
 
   const formIsLoading = ["loading", "submitting"].includes(fetcher.state);
-  
-  useEffect (() => {
-        setClientData(data.client);
-  }, [data.client]);
 
-  
   function handleSubmit(): void {
     const result = ClientSchema.safeParse(clientData);
     if (!result.success) {
@@ -146,10 +151,7 @@ export default function NewClient() {
     }
     setFormErrors(undefined);
 
-    fetcher.submit(
-      clientData,
-      {method: "post", encType: "application/json"},
-    );
+    fetcher.submit(clientData, { method: "post", encType: "application/json" });
   }
 
   const handleClientChange = (value: string, id: string) => {
@@ -161,42 +163,41 @@ export default function NewClient() {
         [id]: value,
       };
     });
-  }
+  };
 
   function handleAddStoreUrl() {
     // @ts-ignore
     setClientData((prev) => {
       if (prev === null) {
         return {
-          stores: [storeUrl]
-        }
+          stores: [storeUrl],
+        };
       } else if (!prev.stores) {
         return {
           ...prev,
-          stores: [storeUrl]
-        }
+          stores: [storeUrl],
+        };
       }
       return {
         ...prev,
-        stores: [...new Set([...prev?.stores, storeUrl])]
-      }
+        stores: [...new Set([...prev?.stores, storeUrl])],
+      };
     });
     setStoreUrl("");
-    setStoreUrlFocus(true)
-
+    setStoreUrlFocus(true);
   }
 
   function handleStoreUrlChange(value: string) {
     setStoreUrl(value);
   }
 
-  function handleSetImageUrl(imageUrl: string) {
-    // @ts-ignore
+  function handleSetImageUrl(imageUrl: string | null) {
+    console.log("Setting Image Url: ", imageUrl);
     setClientData((prev) => {
       return {
         ...prev,
-        imageUrl
-      }
+        imageUrl,
+      };
     });
   }
 
@@ -206,13 +207,13 @@ export default function NewClient() {
       setClientData((prev) => {
         if (prev === null) {
           return {
-            stores: []
-          }
+            stores: [],
+          };
         }
         return {
           ...prev,
-          stores: prev.stores.filter(s => s !== store)
-        }
+          stores: prev.stores.filter((s) => s !== store),
+        };
       });
     },
     [],
@@ -221,7 +222,7 @@ export default function NewClient() {
   return (
     <Page
       title={isNew ? "Add New Client" : "Edit Client"}
-      backAction={{content: "All Clients", url: "/app/clients"}}
+      backAction={{ content: "All Clients", url: "/app/clients" }}
       primaryAction={{
         content: "Save",
         disabled: JSON.stringify(data.client) === JSON.stringify(clientData),
@@ -274,20 +275,33 @@ export default function NewClient() {
               </Card>
               <Card>
                 <FormLayout>
-                  <TextField label="Store Url"
-                             focused={storeUrlFocus}
-                             autoComplete="off"
-                             value={storeUrl}
-                             onBlur={() => setStoreUrlFocus(false)}
-                             type={"url"}
-                             onChange={handleStoreUrlChange}
-                             connectedRight={<Button onClick={handleAddStoreUrl}
-                                                     disabled={storeUrl?.length === 0}>Add</Button>}
-                             prefix={"https://"}>
-                  </TextField>
+                  <TextField
+                    label="Store Url"
+                    focused={storeUrlFocus}
+                    autoComplete="off"
+                    value={storeUrl}
+                    onBlur={() => setStoreUrlFocus(false)}
+                    type={"url"}
+                    onChange={handleStoreUrlChange}
+                    connectedRight={
+                      <Button
+                        onClick={handleAddStoreUrl}
+                        disabled={storeUrl?.length === 0}
+                      >
+                        Add
+                      </Button>
+                    }
+                    prefix={"https://"}
+                  ></TextField>
                   <InlineStack gap={"200"}>
-                    {clientData?.stores?.map(store => (
-                      <Tag onRemove={handleRemoveStoreUrl(store)} key={store} url={`https://${store}`}>{store}</Tag>
+                    {clientData?.stores?.map((store) => (
+                      <Tag
+                        onRemove={handleRemoveStoreUrl(store)}
+                        key={store}
+                        url={`https://${store}`}
+                      >
+                        {store}
+                      </Tag>
                     ))}
                   </InlineStack>
                 </FormLayout>
@@ -296,7 +310,10 @@ export default function NewClient() {
           </Layout.Section>
           <Layout.Section variant="oneThird">
             <FormLayout>
-              <ClientImageUploader imageUrl={clientData?.imageUrl} handleSetImageUrl={handleSetImageUrl}/>
+              <ClientImageUploader
+                imageUrl={clientData?.imageUrl}
+                handleSetImageUrl={handleSetImageUrl}
+              />
               {!isNew && (
                 <Card>
                   <Text as="h2" variant="bodyMd">
@@ -317,4 +334,3 @@ export default function NewClient() {
     </Page>
   );
 }
-
